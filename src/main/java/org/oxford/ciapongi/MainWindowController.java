a6468f3a5e1dcf82d04a89d3c7f8f7201df319c7
maxry <max.ryder8@gmail.com>
2023-04-07 12:25:04 +0200

    Global timer threaded

--- src/main/java/org/oxford/ciapongi/MainWindowController.java
+++ src/main/java/org/oxford/ciapongi/MainWindowController.java
@@ -1,8 +1,25 @@
 package org.oxford.ciapongi;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 
 public class MainWindowController {
+    private static final DoubleProperty globalTime = new SimpleDoubleProperty(System.currentTimeMillis());
+    static {
+        new Thread(() -> {
+            while (true) {
+                globalTime.set(System.currentTimeMillis());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
+    public static DoubleProperty getGlobalTime() {
+        return globalTime;
+    }
 
 }
